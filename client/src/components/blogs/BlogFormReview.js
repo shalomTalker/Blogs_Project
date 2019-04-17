8364743592bbae3b947ae79fdfd3477f2711e810
shalomTalker <shalom604@gmail.com>
2019-04-17 15:13:25 +0300

    first

--- client/src/components/blogs/BlogFormReview.js
+++ client/src/components/blogs/BlogFormReview.js
@@ -7,8 +7,6 @@ import { withRouter } from 'react-router-dom';
 import * as actions from '../../actions';
 
 class BlogFormReview extends Component {
-  state = { file: null };
-
   renderFields() {
     const { formValues } = this.props;
 
@@ -46,11 +44,7 @@ class BlogFormReview extends Component {
 
     const { submitBlog, history, formValues } = this.props;
 
-    submitBlog(formValues, this.state.file, history);
-  }
-
-  onFileChange(event) {
-    this.setState({ file: event.target.files[0] });
+    submitBlog(formValues, history);
   }
 
   render() {
@@ -58,13 +52,6 @@ class BlogFormReview extends Component {
       <form onSubmit={this.onSubmit.bind(this)}>
         <h5>Please confirm your entries</h5>
         {this.renderFields()}
-
-        <h5>Add An Image</h5>
-        <input
-          onChange={this.onFileChange.bind(this)}
-          type="file"
-          accept="image/*"
-        />
 
         {this.renderButtons()}
       </form>
